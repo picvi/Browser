26bec1a19f98c5795e04219c451eff94612021ff
picvi <viki.galaburda@gmail.com>
2020-10-29 23:47:01 +0300

    changed submit function

--- src/forms.js
+++ src/forms.js
@@ -1,41 +1,19 @@
-document.getElementById("submit").addEventListener("click", getUserData);
-const userData = {};
+let userData = {};
 
 function getUserData() {
   const form = document.querySelector("form").elements;
   
   for (let element of form) {
-    switch (element.type) {
-      case "radio":
-        
-        console.log("radio")
-        break;
-      case "select":
-        
-        console.log("select")
-        break;
-      default:
+    if (element.type === "radio") {
+      if (element.checked) {
+          userData[element.name] = element.value;
+        }
+      } else {
         console.log("others")
         userData[element.name] = element.value;
     }
   }
-
-  return userData;
+  console.log(userData);
 }
 
-function getOptionData(elem) {
-  for (let option of elem.options) {
-    if (option.selected) {
-      userData[option.name] = option.value;
-    }
-  }
-}
-
-function getRadioData() {
-  const buttons = document.getElementsByName("sex");
-  for (let button of buttons) {
-    if (button.checked) {
-      userData[button.name] = button.value;
-    }
-  }
-}
+document.getElementById("submit").addEventListener("click", getUserData);

ef7e9bfacba3704a2e4330e9618e14c274629827
picvi <viki.galaburda@gmail.com>
2020-10-29 22:44:36 +0300

    added forms

--- src/forms.js
+++ src/forms.js
@@ -1,16 +1,41 @@
 document.getElementById("submit").addEventListener("click", getUserData);
+const userData = {};
 
 function getUserData() {
   const form = document.querySelector("form").elements;
-  const userData = {};
-
+  
   for (let element of form) {
-    userData[element.name] = element.value;
-
-    if (element.checked) {
-      userData[element.name] = element.value;
+    switch (element.type) {
+      case "radio":
+        
+        console.log("radio")
+        break;
+      case "select":
+        
+        console.log("select")
+        break;
+      default:
+        console.log("others")
+        userData[element.name] = element.value;
     }
   }
 
-  console.log(userData)
+  return userData;
+}
+
+function getOptionData(elem) {
+  for (let option of elem.options) {
+    if (option.selected) {
+      userData[option.name] = option.value;
+    }
+  }
+}
+
+function getRadioData() {
+  const buttons = document.getElementsByName("sex");
+  for (let button of buttons) {
+    if (button.checked) {
+      userData[button.name] = button.value;
+    }
+  }
 }
